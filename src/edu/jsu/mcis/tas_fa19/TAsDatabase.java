bbb01c8ac1ca7138c07b8eaca03f316e05fcdf7a
Woong Kwon <wkwon@stu.jsu.edu>
2019-10-31 13:20:24 -0500

    Working on Database

---
+++ src/edu/jsu/mcis/tas_fa19/TAsDatabase.java
@@ -0,0 +1,17 @@
+package edu.jsu.mcis.tas_fa19;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+public class TAsDatabase {
+    
+    Connection conn;
+    
+    String url = "jdbc:mysql://localhost/tas";
+    String username = "CS310";
+    String password = "CS310";
+    
+    
+}
