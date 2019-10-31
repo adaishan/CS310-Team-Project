3d823a8f67e26c1ae51679152c444058778ca2d4
Matthew Allen <mallen3@stu.jsu.edu>
2019-10-31 14:01:45 -0500

    Added Punch Class to TASDatabase Class

--- src/edu/jsu/mcis/tas_fa19/TAsDatabase.java
+++ src/edu/jsu/mcis/tas_fa19/TAsDatabase.java
@@ -7,20 +7,54 @@ import java.util.GregorianCalendar;
 
 public class TAsDatabase {
     
+public Punch getPunch(String ID){
+    {
     
-    
-    Connection conn;
-    
-    String url = "jdbc:mysql://localhost/tas";
-    String username = "CS310";
-    String password = "CS310";
-    
-    Class.forname("com.mysql.jdbc.Driver");
-    
-    
-    
+    private int ID;
+    private String badgeID;
+    private int terminalID;
+    private int punch_typeID;
+    private GregorianCalendar original_time_stamp = new GregorianCalendar();
 
-public Badge getBadge(String id){
+    public int getID() {
+        return ID;
+    }
+
+    public String getBadgeID() {
+        return badgeID;
+    }
+
+    public int getTerminalID() {
+        return terminalID;
+    }
+
+    public int getPunch_typeID() {
+        return punch_typeID;
+    }
+
+    public GregorianCalendar getOriginal_time_stamp() {
+        return original_time_stamp;
+    }
+    
+    public String toString(){
+
+        //("#12565C60 (Chapman, Joshua E)"
+        StringBuilder s = new StringBuilder();
+        s.append("#");
+        s.append(ID);
+        s.append(" (");
+        s.append(terminalID);
+        s.append(")");
+        s.append(badgeID);
+        s.append(original_time_stamp);
+        s.append(punch_typeID);
+        return s.toString();
+        
+       
+    }
+}  
+
+public Badge getBadge(String ID){
 
 }
 
