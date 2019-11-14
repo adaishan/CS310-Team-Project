05705e7f2cfc77d69e75fc7248acf102f9a8c251
Matthew Allen <mallen3@stu.jsu.edu>
2019-11-14 14:16:10 -0600

    Got Feature 1 Passing Unit Test

--- src/edu/jsu/mcis/tas_fa19/TASDatabase.java
+++ src/edu/jsu/mcis/tas_fa19/TASDatabase.java
@@ -130,14 +130,93 @@ public class TASDatabase {
         
     }
     
-     public Shift getShift(int shiftid) {
+     public Shift getShift(int id) {
+         
         Shift s = null;
+        
+        try {
+            
+            String query = "SELECT * FROM shift WHERE id = ?";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setInt(1, id);
+            
+            boolean hasresults = ps.execute();
+            
+            if (hasresults) {
+                
+                ResultSet resultset = ps.getResultSet();
+                
+                if (resultset.next()) {
+                    
+                    HashMap<String, Object> params = new HashMap<>();
+                    
+                    params.put("id", String.valueOf(id));
+                    params.put("interval", String.valueOf(resultset.getInt("interval")));
+                    params.put("graceperiod", String.valueOf(resultset.getInt("graceperiod")));
+                    params.put("dock", String.valueOf(resultset.getInt("dock")));
+                    params.put("lunchdeduct", String.valueOf(resultset.getInt("lunchdeduct")));
+                    
+                    params.put("start", resultset.getTime("start"));
+                    params.put("stop", resultset.getTime("stop"));
+                    params.put("lunchstart", resultset.getTime("lunchstart"));
+                    params.put("lunchstop", resultset.getTime("lunchstop"));
+                    
+                    params.put("description", resultset.getString("description"));                    
+                    
+                    s = new Shift(params);
+                    
+                }
+                
+                resultset.close();
+                
+            }
+            
+            ps.close();
+            
+        }
+        catch (Exception e) { e.printStackTrace(); }
+        
         return s;
+        
     }
      
     public Shift getShift(Badge b) {
+        
         Shift s = null;
+        
+        try {
+            
+            String query = "SELECT * FROM employee WHERE badgeid = ?";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, b.getId());
+            
+            boolean hasresults = ps.execute();
+            
+            if (hasresults) {
+                
+                ResultSet resultset = ps.getResultSet();
+                
+                if (resultset.next()) {
+                    
+                    int shiftid = resultset.getInt("shiftid");
+                    
+                    s = getShift(shiftid);
+                    
+                }
+                
+                resultset.close();
+                
+            }
+            
+            ps.close();
+            
+        }
+        catch (Exception e) { e.printStackTrace(); }
+        
         return s;
+        
     }
     
 }
