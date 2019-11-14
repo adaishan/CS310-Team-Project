d3f7183579f02f7bd8fe07c6f43d51b4e755ff9f
Matthew Allen <mallen3@stu.jsu.edu>
2019-11-14 14:19:45 -0600

    Got Feature 1 Passing Unit Test

--- src/edu/jsu/mcis/tas_fa19/Shift.java
+++ src/edu/jsu/mcis/tas_fa19/Shift.java
@@ -1,92 +1,59 @@
 package edu.jsu.mcis.tas_fa19;
 
 import java.time.LocalTime;
+import java.util.HashMap;
+import java.sql.Time;
+import java.time.temporal.ChronoUnit;
 
 public class Shift {
     
     private int id, interval, graceperiod, lunchdeduct, dock;
-    private int shiftduration, lunchduration;
     private String description;
     private LocalTime start, stop, lunchstart, lunchstop;
 
-    public int getId() {
-        return id;
-    }
-    public int getInterval() {
-        return interval;
-    }
-    public int getGraceperiod() {
-        return graceperiod;
-    }
-    public int getLunchdeduct() {
-        return lunchdeduct;
-    }
-    public int getDock() {
-        return dock;
-    }
-    public int shiftduration() {
-        return shiftduration;
-    }
-    public int getLunchduration() {
-        int lunchduration = (int)(((lunchstop.compareTo(lunchstart))));
-        return lunchduration;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public LocalTime getStart() {
-        return start;
-    }
-    public LocalTime getStop() {
-        return stop;
-    }
-    public LocalTime getLunchstart() {
-        return lunchstart;
-    }
-    public LocalTime getLunchstop() {
-        return lunchstop;
-    }
-   
     
-    public Shift(int id, String description, LocalTime start, LocalTime stop, int shiftduration, int interval, int graceperiod, int dock, LocalTime lunchstart, LocalTime lunchstop, int lunchdeduct, int lunchduration) {
-        this.id = id;
-        this.description = description;
-        this.start = start;
-        this.stop = stop;
-        this.shiftduration = shiftduration;
-        this.interval = interval;
-        this.graceperiod = graceperiod;
-        this.dock = dock;
-        this.lunchstart = lunchstart;
-        this.lunchstop = lunchstop;
-        this.lunchdeduct = lunchdeduct;
-        this.lunchduration = lunchduration;
+    public Shift(HashMap<String, Object> params) {
+        
+        this.id = Integer.parseInt((String)params.get("id"));
+        this.interval = Integer.parseInt((String)params.get("interval"));
+        this.graceperiod = Integer.parseInt((String)params.get("graceperiod"));
+        this.dock = Integer.parseInt((String)params.get("dock"));
+        this.lunchdeduct = Integer.parseInt((String)params.get("lunchdeduct"));
+        
+        this.start = ((Time)params.get("start")).toLocalTime();
+        this.stop = ((Time)params.get("stop")).toLocalTime();
+        this.lunchstart = ((Time)params.get("lunchstart")).toLocalTime();
+        this.lunchstop = ((Time)params.get("lunchstop")).toLocalTime();
+        
+        this.description = (String)params.get("description");
+        
+        
     }
     
     //assertEquals("Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)", s1.toString());
     
-    public String toString(){
+    public String toString() {
         
-    StringBuilder s = new StringBuilder();
-    s.append (description);
-    s.append (": ");
-    s.append (start);
-    s.append (" - ");
-    s.append (stop);
-    s.append (" (");
-    s.append (stop.compareTo(start));
-    s.append (" minutes");
-    s.append (") ");
-    s.append ("; ");
-    s.append ("Lunch: ");
-    s.append (lunchstart);
-    s.append (" - ");
-    s.append (lunchstop);
-    s.append (" (");
-    s.append (lunchduration);
-    s.append (" minutes");
-    s.append (") ");
-    return s.toString();
+        // "Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)"
+        
+        StringBuilder s = new StringBuilder();
+        s.append (description);
+        s.append (": ");
+        s.append (String.format("%02d", start.getHour())).append(":").append(String.format("%02d", start.getMinute()));
+        s.append (" - ");
+        s.append (String.format("%02d", stop.getHour())).append(":").append(String.format("%02d", stop.getMinute()));
+        s.append (" (");
+        s.append ( ChronoUnit.MINUTES.between(start, stop) );
+        s.append (" minutes").append (")").append("; ").append ("Lunch: ");
+        s.append (String.format("%02d", lunchstart.getHour())).append(":").append(String.format("%02d", lunchstart.getMinute()));
+        s.append (" - ");
+        s.append (String.format("%02d", lunchstop.getHour())).append(":").append(String.format("%02d", lunchstop.getMinute()));
+        s.append (" (");
+        s.append ( ChronoUnit.MINUTES.between(lunchstart, lunchstop) );
+        s.append (" minutes");
+        s.append (")");
+        
+        return s.toString();
         
     }
 }
